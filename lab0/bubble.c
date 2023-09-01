0b50e4a1b75b2c8793b9d48a36aded1f957ae207
Vrushank Agrawal <vrushank2001@gmail.com>
2023-08-31 23:59:42 -0400

    add bubble sort

--- lab0/bubble.c
+++ lab0/bubble.c
@@ -1,13 +1,28 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+void swap(int* prev, int* next) {
+    int temp = *prev;
+    *prev = *next;
+    *next = temp;
+}
+
 
 int *bubble(int *input, int n) {
     /***** YOUR CODE HERE *****/
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (input[j] > input[j+1])
+                swap(&input[j], &input[j+1]);
+        }
+    }
+    return input;
 }
 
 // verify() checks that the array is sorted
-// DO NOT call verify() when profiling your code 
+// DO NOT call verify() when profiling your code
 int verify(int *input, int n) {
     for (int i = 1; i < n; i++) {
         assert(input[i] >= input[i-1]);
@@ -24,7 +39,7 @@ int main(int argc, char **argv) {
     int *input = (int *) malloc(sizeof(int)*n);
     int *output; // you will need to allocate memory for output if not sorting in place
     FILE *fp = fopen(argv[2], "r");
-    
+
     for (int i = 0; i < n; i++) {
         fscanf(fp, "%d,", &input[i] );
     }
