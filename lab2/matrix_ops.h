b146c2d081cec6abd503aea33263317b2046c75f
Vrushank Agrawal <vrushank2001@gmail.com>
2023-10-03 15:25:40 -0400

    initialize lab2

---
+++ lab2/matrix_ops.h
@@ -0,0 +1,10 @@
+#ifndef MATRIX_OPS_H
+#define MATRIX_OPS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+float **matmul(float **A, float **B, int A_rows, int A_cols, int B_rows, int B_cols);
+float **matmul_blocking(float **A, float **B, int A_rows, int A_cols, int B_rows, int B_cols, int block_size);
+
+#endif /* MATRIX_OPS_H */
